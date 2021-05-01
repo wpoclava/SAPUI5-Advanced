8292684d4071aa1cac2fa789ed812ac2bd33ea2e
wpoclava <wpoclava@gmail.com>
2021-05-01 05:14:33 +0000

    03 Modelos y Eventos

--- Employees/webapp/controller/MainView.controller.js
+++ Employees/webapp/controller/MainView.controller.js
@@ -1,28 +1,100 @@
 sap.ui.define([
-		"sap/ui/core/mvc/Controller"
-	],
+    "sap/ui/core/mvc/Controller",
+    "sap/ui/model/Filter",
+    "sap/ui/model/FilterOperator"
+],
 	/**
      * @param {typeof sap.ui.core.mvc.Controller} Controller
+     * @param {typeof sap.ui.model.Filter} Filter
+     * @param {typeof sap.ui.model.FilterOperator} FilterOperator
      */
-	function (Controller) {
+    function (Controller, Filter, FilterOperator) {
         "use strict";
 
-        var Main = Controller.extend("logaligroup.Employees.controller.MainView", {});
-        
-        Main.prototype.onValidate = function () {
-                var inputEmployee = this.byId("inputEmployee");
-                var valueEmployee = inputEmployee.getValue();
+        function onInit() {
+            var oJsonModel = new sap.ui.model.json.JSONModel();
+            var oView = this.getView();
+            var i18nBundle = oView.getModel("i18n").getResourceBundle();
 
-                if (valueEmployee.length === 6) {
-                    //inputEmployee.setDescription("OK");
-                    this.getView().byId("labelCountry").setVisible(true);
-                    this.getView().byId("slCountry").setVisible(true);
-                } else {
-                    //inputEmployee.setDescription("Not OK");
-                    this.getView().byId("labelCountry").setVisible(false);
-                    this.getView().byId("slCountry").setVisible(false);                    
-                }                
-            };
+            // var oJSON = {
+            //     employeeId: "12345",
+            //     countryKey: "UK",
+            //     listCountry: [
+            //         {
+            //             key: "US",
+            //             text: i18nBundle.getText("countryUS")
+            //         },
+            //         {
+            //             key: "UK",
+            //             text: i18nBundle.getText("countryUK")
+            //         },
+            //         {
+            //             key: "ES",
+            //             text: i18nBundle.getText("countryES")
+            //         }
+            //     ]
+            // };
+
+            //oJsonModel.setData(oJSON);
+
+            oJsonModel.loadData("./localService/mockdata/Employees.json", false);
+            // oJsonModel.attachRequestCompleted(function (oEventModel) { 
+            //     console.log(JSON.stringify(oJsonModel.getData()));
+            // })
+            oView.setModel(oJsonModel);
+
+        };
+
+        function onFilter() {
+
+            var oJSON = this.getView().getModel().getData();
+
+            var filters = [];
+
+            if (oJSON.EmployeeId !== "") {
+                filters.push(new Filter("EmployeeID", FilterOperator.EQ, oJSON.EmployeeId));
+            }
+
+            if (oJSON.CountryKey !== "") {
+                filters.push(new Filter("Country", FilterOperator.EQ, oJSON.CountryKey));
+            }
+
+            var oList = this.getView().byId("tableEmployee");
+            var oBinding = oList.getBinding("items");
+            oBinding.filter(filters);
+
+        };
+
+        function onClearFilter() {
+
+            var oModel = this.getView().getModel();
+            oModel.setProperty("/EmployeeId", "");
+            oModel.setProperty("/CountryKey", "");
+
+        }
+
+        var Main = Controller.extend("logaligroup.Employees.controller.MainView", {});
+
+        Main.prototype.onValidate = function () {
+            var inputEmployee = this.byId("inputEmployee");
+            var valueEmployee = inputEmployee.getValue();
+
+            if (valueEmployee.length === 6) {
+                //inputEmployee.setDescription("OK");
+                this.getView().byId("labelCountry").setVisible(true);
+                this.getView().byId("slCountry").setVisible(true);
+            } else {
+                //inputEmployee.setDescription("Not OK");
+                this.getView().byId("labelCountry").setVisible(false);
+                this.getView().byId("slCountry").setVisible(false);
+            }
+        };
+
+        Main.prototype.onInit = onInit;
+
+        Main.prototype.onFilter = onFilter;
+        
+        Main.prototype.onClearFilter = onClearFilter;
 
         return Main;
-	});
+    });
